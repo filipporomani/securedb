ce1684cc0c9fae4367a3a253348786c258f078a3
Filippo Romani <filipporomanionline@gmail.com>
2022-11-23 17:13:30 +0100

    fix wrong version name

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='securedb',
-    version='2.0.0',
+    version='2.0.1',
     description='securedb is a fast and lightweight Python framework to easily interact with JSON-based encrypted databases.',
     py_modules=["securedb"],
     package_dir={'': 'src'},
